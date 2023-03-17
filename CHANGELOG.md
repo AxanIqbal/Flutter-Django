bd08dde10b3c01de4354ca603ec8755a25d0335a
Ahsan Iqbal <axan.iqbal1234@gmail.com>
2023-03-17 20:24:59 +0500

    dio connection error

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.1.1
+- change Connection Error to Built-in `DioErrorType.connectionError`
+
 ## 1.1.0
 - Remove `flutter_secure_storage`
 - Flutter package to Dart package
--- lib/src/extension/extension.dart
+++ lib/src/extension/extension.dart
@@ -1,10 +1,8 @@
-import 'dart:io';
-
 import 'package:dio/dio.dart';
 
 extension DioErrorX on DioError {
   bool get isNoConnectionError {
-    return type == DioErrorType.connectionError && error is SocketException;
+    return type == DioErrorType.connectionError;
   }
 
   String get serverErrorOrConnection {
